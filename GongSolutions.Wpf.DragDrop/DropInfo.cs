eacc7d8b505fe63b92e479002dd03f7a10034a43
Thomas Matelich <thomas@mateli.ch>
2014-10-09 12:44:27 -0700

    This prevents nested drop targets, which I want

--- GongSolutions.Wpf.DragDrop/DropInfo.cs
+++ GongSolutions.Wpf.DragDrop/DropInfo.cs
@@ -42,7 +42,7 @@ namespace GongSolutions.Wpf.DragDrop
 
       this.VisualTarget = sender as UIElement;
       // if drop target isn't a ItemsControl
-      if (!(this.VisualTarget is ItemsControl)) {
+      /*if (!(this.VisualTarget is ItemsControl)) {
         // try to find next ItemsControl
         var itemsControl = VisualTreeExtensions.GetVisualAncestor<ItemsControl>(this.VisualTarget);
         if (itemsControl != null) {
@@ -51,7 +51,7 @@ namespace GongSolutions.Wpf.DragDrop
             this.VisualTarget = itemsControl;
           }
         }
-      }
+      }*/
       // visual target can be null, so give us a point...
       this.DropPosition = this.VisualTarget != null ? e.GetPosition(this.VisualTarget) : new Point();
 
